c10a2fb733172309bf9b77517c4cc72aece3edf0
MugeraH <bwosihughes24@gmail.com>
2021-03-02 23:36:20 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append(`<li><span class='contact'> ${newContact.fullName()} </span></li>`);
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+    $('.name').slideUp(100)
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });  
+  });
+
+
+
+}); 
+
+
+
+//? a prototype method is a method meant to be called on a specific type of object.
